19253441b2508f2d2bb488173257ba886066cb0e
tata0409 <tatachechyna@gmail.com>
2025-01-03 20:25:43 -0500

    Prediction on linear regression about energy usage

--- main.py
+++ main.py
@@ -4,35 +4,21 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
 
-data = {
-    "Days": [1, 2, 3, 4, 5],
-    "Candies": [2, 4, 6, 8, 10]
-}
+np.random.seed(24)
 
-
-df = pd.DataFrame(data)
-print(df)
-
-plt.scatter(df["Days"], df["Candies"], color="blue")
-plt.title("Candies/Days")
-plt.xlabel("Days")
-plt.ylabel("Candies")
-#plt.show()
-
-X = df[["Days"]]
-Y = df["Candies"]
+hours = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9])
+energy_usage = 10 * hours + 15 + np.random.normal(0, 5, len(hours))
+X = hours.reshape(-1, 1)
+Y = energy_usage
 model = LinearRegression()
 model.fit(X, Y)
 Y_pred = model.predict(X)
-print(f"W: {model.coef_}\nb: {model.intercept_}")
+print(f"W: {model.coef_[0]}\nb: {model.intercept_}")
 
 plt.scatter(X, Y, color="blue")
 plt.plot(X, Y_pred, color="red")
 plt.title("Linear Regression")
-plt.xlabel("Days")
-plt.ylabel("Candies")
+plt.xlabel("Hours")
+plt.ylabel("Energy usage")
 plt.show()
 
-new_value = [[10]]
-predicted_candy = model.predict(new_value)
-print(f"Predicted for the {new_value[0][0]} day: {predicted_candy[0]}")
